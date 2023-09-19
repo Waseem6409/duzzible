b7f641fb32f308a7a576d2776df8cc96db3f8b6a
Waseem Munir <waseem6409@gmail.com>
2023-09-19 05:28:40 +0500

    clean

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Octicon from "react-octicon";
 import { formatDate } from "../utils/utils";
+import styles from "./GistList.module.css";
 
 const GistList = ({ gists }) => (
   <Wrapper>
@@ -18,32 +19,12 @@ const GistList = ({ gists }) => (
             if (newWindow) newWindow.opener = null;
           }}
         >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <img
-                src={item?.owner?.avatar_url}
-                style={{
-                  width: 30,
-                  height: 30,
-                  borderRadius: 30 / 2,
-                }}
-                alt={item?.id}
-              />
-              <h6 style={{ marginLeft: "10px" }}>{item?.owner?.login}</h6>
+          <div className={styles.top}>
+            <div className={styles.profileContainer}>
+              <img src={item?.owner?.avatar_url} alt={item?.id} />
+              <h6>{item?.owner?.login}</h6>
             </div>
-            <div style={{ display: "flex", alignItems: "center" }}>
+            <div className={styles.fileContainer}>
               <Octicon name="file" />
               <h6>
                 {item?.files ? Object.keys(item?.files)?.length : null}{" "}
@@ -55,46 +36,27 @@ const GistList = ({ gists }) => (
               </h6>
             </div>
           </div>
-          <div style={{ display: "flex" }}>
-            <h6 style={{ margin: 0 }}>
-              Created at {formatDate(item?.created_at)}
-            </h6>
-            <h6 style={{ margin: 0, marginLeft: 10 }}>
-              Updated at {formatDate(item?.updated_at)}
-            </h6>
+          <div className={styles.dateContainer}>
+            <h6>Created at {formatDate(item?.created_at)}</h6>
+            <h6>Updated at {formatDate(item?.updated_at)}</h6>
           </div>
-          <div>
-            <h6
-              style={{
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
+          <div className={styles.descriptionContainer}>
+            <h6>
               {item?.description !== null
                 ? item?.description
                 : "No description"}
             </h6>
           </div>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-            }}
-          >
+          <div className={styles.fileListContainer}>
             {Object.keys(item?.files).map((item, i) => {
               return (
                 <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginRight: "10px",
-                    overflow: "hidden",
-                  }}
+                className={styles.file}
+                
                   key={i}
                 >
                   <Octicon name="file" />
-                  <h6 style={{ margin: "10px 0" }}>{item}</h6>
+                  <h6 >{item}</h6>
                 </div>
               );
             })}
